5cca16018f1a79034f1e9d1b96186ae390a8ee77
jnowat <65848679+jnowat@users.noreply.github.com>
2025-05-14 10:41:21 -0700

    Update indi_astrospheric_weather.h
    Broader revisions, actually compiles;
    doesn't show up in INDI control panel

--- drivers/weather/indi_astrospheric_weather.h
+++ drivers/weather/indi_astrospheric_weather.h
@@ -4,23 +4,26 @@
  This library is free software; you can redistribute it and/or
  modify it under the terms of the GNU Library General Public
  License version 2 as published by the Free Software Foundation.
- .
+
  This library is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  Library General Public License for more details.
- .
+
  You should have received a copy of the GNU Library General Public License
  along with this library; see the file COPYING.LIB.  If not, write to
  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
  Boston, MA 02110-1301, USA.
+
+ Contributions:
+ - Grok-3 (xAI)
+ - GPT-4o (OpenAI)
 *******************************************************************************/
 
 #ifndef INDI_ASTROSPHERIC_WEATHER_H
 #define INDI_ASTROSPHERIC_WEATHER_H
 
 #include <indiweather.h>
-#include <indipropertytext.h>
 #include <vector>
 #include <string>
 #include <ctime>
@@ -34,15 +37,13 @@ public:
     virtual const char *getDefaultName() override;
     virtual bool initProperties() override;
     virtual bool ISNewText(const char *dev, const char *name, char *texts[], char *names[], int n) override;
-    virtual bool loadConfig(bool silent = false, const char *property = nullptr) override;
-    virtual bool saveConfig(bool silent = false, const char *property = nullptr) override;
+    virtual bool saveConfigItems(FILE *fp) override;
 
 protected:
     virtual IPState updateWeather() override;
 
 private:
-    INDI::PropertyText APIKeyT[1];
-    INDI::PropertyTextVectorProperty APIKeyTP;
+    INDI::PropertyText APIKeyTP{1};
 
     std::vector<double> cloudCover, temperature, windSpeed, dewPoint, windDirection, seeing, transparency;
     time_t forecastStartTime;
@@ -53,7 +54,7 @@ private:
 
     bool fetchDataFromAPI(std::string &responseBody);
     bool parseJSONResponse(const std::string &jsonResponse);
-    static time_t parseUTCDateTime(const std::string& dateTimeStr);
+    static time_t parseUTCDateTime(const std::string &dateTimeStr);
 };
 
 #endif // INDI_ASTROSPHERIC_WEATHER_H
